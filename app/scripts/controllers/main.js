1664742fd44daffceba5fb59f820b217d9ff670d
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 20:31:10 +1000

    main controller added

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name githubDirectoryApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the githubDirectoryApp
+ */
+angular.module('githubDirectoryApp')
+  .controller('MainCtrl', ['$scope',function ($scope) {
+
+  }]);
