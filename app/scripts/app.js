52742e24ee7d494bba4a7ba65754b600b9e482a3
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 20:48:53 +1000

    setting the routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,5 +16,22 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch'
-  ])
-;
+  ]).config(['$routeProvider', function ($routeProvider) {
+
+    var routes = [
+      {
+        url: '/',
+        config: {
+          template: '<app-info></app-info>'
+        }
+      }
+    ];
+
+    routes.forEach(function (route) {
+      $routeProvider.when(route.url, route.config);
+    });
+
+    $routeProvider.otherwise({redirectTo: '/'});
+
+  }]);
+
