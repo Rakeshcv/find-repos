0e42c501471100a7e72f541fcf561679a530f9ab
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 20:32:27 +1000

    directive added

---
+++ app/scripts/directives/appinfo.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name githubDirectoryApp.directive:appInfo
+ * @description
+ * # appInfo
+ */
+angular.module('githubDirectoryApp')
+  .directive('appInfo', function () {
+    return {
+
+    };
+  });
---
+++ test/spec/directives/appinfo.js
@@ -0,0 +1,20 @@
+//'use strict';
+//
+//describe('Directive: appInfo', function () {
+//
+//  // load the directive's module
+//  beforeEach(module('githubDirectoryApp'));
+//
+//  var element,
+//    scope;
+//
+//  beforeEach(inject(function ($rootScope) {
+//    scope = $rootScope.$new();
+//  }));
+//
+//  it('should make hidden element visible', inject(function ($compile) {
+//    element = angular.element('<app-info></app-info>');
+//    element = $compile(element)(scope);
+//    expect(element.text()).toBe('this is the appInfo directive');
+//  }));
+//});
