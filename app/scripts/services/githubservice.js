f40194432cb15d76904add19f3f7a8320f98fc51
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 21:54:24 +1000

    retrieves repo details for given username

--- app/scripts/services/githubservice.js
+++ app/scripts/services/githubservice.js
@@ -8,6 +8,26 @@
  * Service in the githubDirectoryApp.
  */
 angular.module('githubDirectoryApp')
-  .service('githubservice', function () {
+  .service('githubservice', ['$http',function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+
+    var getUser = function(query){
+      return $http('https://api.github.com/users/' + query).then(function (response) {
+        console.log(response.data);
+        return response.data;
+      });
+    };
+
+    var getRepos = function(){
+      return $http.get(query).then(function (response) {
+        return response.data;
+      });
+    };
+
+    return{
+      getUser: getUser,
+      getRepos: getRepos
+    };
+
+
+  }]);
