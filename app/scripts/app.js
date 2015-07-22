5957335200a18a00b8146a3cf9b978267ab187cc
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 20:30:04 +1000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name githubDirectoryApp
+ * @description
+ * # githubDirectoryApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('githubDirectoryApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+;
