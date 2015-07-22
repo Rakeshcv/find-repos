1d4ae54bdfcb7ca3130641b2067781ec1a5320d5
Rakesh C V <rakesh.cv@hotmail.com>
2015-07-22 20:33:44 +1000

    githubservice added

---
+++ app/scripts/services/githubservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name githubDirectoryApp.githubservice
+ * @description
+ * # githubservice
+ * Service in the githubDirectoryApp.
+ */
+angular.module('githubDirectoryApp')
+  .service('githubservice', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ test/spec/services/githubservice.js
@@ -0,0 +1,18 @@
+//'use strict';
+//
+//describe('Service: githubservice', function () {
+//
+//  // load the service's module
+//  beforeEach(module('githubDirectoryApp'));
+//
+//  // instantiate service
+//  var githubservice;
+//  beforeEach(inject(function (_githubservice_) {
+//    githubservice = _githubservice_;
+//  }));
+//
+//  it('should do something', function () {
+//    expect(!!githubservice).toBe(true);
+//  });
+//
+//});
